45112a4f591e39d930205532689bf18d27d6f8f1
Abdelrahman Ismail <dev@ismail9k.com>
2021-10-25 23:09:31 +0300

    feat: update info

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,43 +4,42 @@ include ../fx-mixin.pug
 Layout
   section.intro
     .intro-info
-      h2.intro-title.animation(data-animation="slide-up")
-        |I'm Abdelrahman Ismail, a full-stack web developer, writer, and pro gamer.
-    .intro-figure.animation(data-animation="bounce")
+      h2.intro-title.animation(data-animation='slide-up')
+        | I'm Abdelrahman Ismail, a full-stack engineer, writer, and pro gamer.
+    .intro-figure.animation(data-animation='bounce')
       Figure
 
-
   section#working.section
-    .section-info.animation(data-animation="slide-up"):  .section-sticky
+    .section-info.animation(data-animation='slide-up'): .section-sticky
       h2.section-title.fx
         +fx('Wor-', 'king')
     .section-elements
-      .card.animation(data-animation="slide-up")
+      .card.animation(data-animation='slide-up')
         h3.card-title Summary
         p.card-description
           | {{ summary.summary }}
         h3.card-title Skills
         p.card-description
-          | {{ skills.expert.join(', ') }}
-        a.resume-button.link(href="/resume" target="_blank" rel="noopener").
+          | {{ skills.expert.join(", ") }}
+        a.resume-button.link(href='/resume', target='_blank', rel='noopener').
           Complete Résumé
 
   section#projects.section
-    .section-info.animation(data-animation="slide-up"): .section-sticky
+    .section-info.animation(data-animation='slide-up'): .section-sticky
       h2.section-title.fx
         +fx('Pro-', 'jects')
 
       p.section-description.
         I'm pleasing being part of these awesome projects, which aim to give a better experience for both developer and end-user.
     .section-elements
-      .card.animation(data-animation="slide-up" v-for="project in projects")
+      .card.animation(data-animation='slide-up', v-for='project in projects')
         h3.card-title
-          a.link(:href="project.link" target="_blank" rel="noopener") {{ project.title }}
+          a.link(:href='project.link', target='_blank', rel='noopener') {{ project.title }}
         h4.card-subtitle {{ project.type }}
         p.card-description {{ project.description }}
 
   section#writings.section
-    .section-info.animation(data-animation="slide-up"): .section-sticky
+    .section-info.animation(data-animation='slide-up'): .section-sticky
       h2.section-title.fx
         +fx('Wri-', 'tings')
       p.section-description.
@@ -48,22 +47,25 @@ Layout
         500+ followers with 35k+ total views, and four badges.#[br]
         I write generally about front-end development techniques and tips.
     .section-elements
-      .card.animation(data-animation="slide-up" v-for="post in posts")
+      .card.animation(data-animation='slide-up', v-for='post in posts')
         h3.card-title
-          a.link(:href="post.link" target="_blank" rel="noopener") {{ post.title }}
+          a.link(:href='post.link', target='_blank', rel='noopener') {{ post.title }}
         p.card-description {{ post.excerpt }}
-        a.card-button(:href="post.link" target="_blank" rel="noopener") read more
+        a.card-button(:href='post.link', target='_blank', rel='noopener') read more
 
   section#contact.section.is-center
-    .section-info.animation(data-animation="slide-up"): .section-sticky
+    .section-info.animation(data-animation='slide-up'): .section-sticky
       h2.section-title.fx
         +fx('Con-', 'tact')
       p.section-description.
         It's your turn now, get in touch. Tell me about your self and how I can help you.
-    .section-elements.animation(data-animation="slide-up")
-      a.contact-button.link(href="https://twitter.com/ismail_9k" target="_blank" rel="noopener").
+    .section-elements.animation(data-animation='slide-up')
+      a.contact-button.link(
+        href='https://twitter.com/ismail_9k',
+        target='_blank',
+        rel='noopener'
+      ).
         Feel free to send me a massage on twitter!
-
 </template>
 
 <script>
@@ -94,8 +96,8 @@ export default {
   methods: {
     initObserver() {
       this.observer = new IntersectionObserver(
-        entries => {
-          entries.forEach(entry => {
+        (entries) => {
+          entries.forEach((entry) => {
             if (entry.isIntersecting) {
               this.startAnimating(entry.target);
             }
@@ -104,10 +106,10 @@ export default {
         { threshold: 0.2 }
       );
 
-      Array.from(document.querySelectorAll('.animation')).forEach(el => {
+      Array.from(document.querySelectorAll('.animation')).forEach((el) => {
         this.observer.observe(el);
       });
-      Array.from(document.querySelectorAll('.fx')).forEach(el => {
+      Array.from(document.querySelectorAll('.fx')).forEach((el) => {
         this.observer.observe(el);
       });
     },
@@ -133,11 +135,11 @@ export default {
       const touchDuration = 1000; //length of time we want the user to touch before we do something
       let timer;
 
-      const goToPlayground = e => {
+      const goToPlayground = (e) => {
         e.preventDefault();
         window.open('/playground');
       };
-      const touchstart = e => {
+      const touchstart = (e) => {
         timer = setTimeout(() => goToPlayground(e), touchDuration);
       };
       const touchend = () => {
