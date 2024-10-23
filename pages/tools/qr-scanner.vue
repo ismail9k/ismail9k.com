427a0d3e20be7e9e66bb535a7b61b0c849a2d25b
Ismail9k <dev@ismail9k.com>
2024-10-23 20:56:49 +0300

    Feat add qr validation tool (#50)
    * feat: add qr scanner tool
    
    * feat: enhance qr scanner feature
    
    * feat: refactor qr scanner add rate limiter for validation request
    
    * feat: add qr scanner tool

--- pages/tools/qr-scanner.vue
+++ pages/tools/qr-scanner.vue
@@ -1,14 +1,49 @@
-<script >
-</script>
-
 <template>
   <div class="qr-scanner-container">
-    <QRCodeScanner />
+    <QRCodeScanner :onScan="validateUrlSafety" />
+    <div v-if="safetyStatus">{{ safetyStatus }}</div>
   </div>
 </template>
+
+<script setup>
+const safetyStatus = ref(null);
+
+const validateUrlSafety = async (url) => {
+  try {
+    const response = await fetch('/api/validate-url', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ url }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data?.error || data);
+    }
+    safetyStatus.value = data.message;
+    if (data.status === 'Safe') {
+      window.location.href = url; // Redirect to the safe URL
+    }
+  } catch (error) {
+    console.error(error);
+    safetyStatus.value = error.message || 'Error validating URL';
+  }
+};
+</script>
+
 
 <style>
 .qr-scanner-container {
   margin: auto;
+  margin: 20px auto;
+  padding: 10px;
+  border: 1px solid black;
+  background: white;
+  color: black;
+  min-width: 300px;
+  text-align: center;
 }
 </style>
