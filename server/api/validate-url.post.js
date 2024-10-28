648bfc95445d7290a7b1538cb9cd599f62575c97
ismail9k <dev@ismail9k.com>
2024-10-28 17:34:02 +0300

    feat: use rate limiter and mongodb

--- server/api/validate-url.post.js
+++ server/api/validate-url.post.js
@@ -1,29 +1,25 @@
 import fetch from 'node-fetch';
-import memoryCache from 'memory-cache';
 
-const RATE_LIMIT_DURATION = 3600000; // 1 hour in milliseconds
-const MAX_REQUESTS_PER_IP = 5;
+const { GOOGLE_SAFE_BROWSING_API_KEY: apiKey } = process.env;
 
-function rateLimit(req) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const requestCount = memoryCache.get(ip) || 0;
+export default defineEventHandler(async (event) => {
+  try {
+    // Extract URL from request body
+    const body = await readBody(event);
+    const { url } = body;
 
-  if (requestCount >= MAX_REQUESTS_PER_IP) {
-    return {
-      status: 429,
-      message: 'Too many QR scans, please try again after an hour',
-    };
+    if (!url) {
+      return { error: 'URL is required' };
+    }
+
+    // Validate the URL via Google Safe Browsing API
+    const validationResult = await validateUrlSafety(url);
+    return validationResult;
+  } catch (error) {
+    return error;
   }
+});
 
-  // Increment request count and store it in cache for 1 hour
-  memoryCache.put(ip, requestCount + 1, RATE_LIMIT_DURATION);
-  return { status: 200 };
-}
-
-// Google Safe Browsing API key
-const apiKey = process.env.GOOGLE_SAFE_BROWSING_API_KEY;
-
-// Call the Google Safe Browsing API to validate URLs
 async function validateUrlSafety(url) {
   const apiUrl = `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${apiKey}`;
 
@@ -33,7 +29,12 @@ async function validateUrlSafety(url) {
       clientVersion: '1.0',
     },
     threatInfo: {
-      threatTypes: ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE'],
+      threatTypes: [
+        'MALWARE',
+        'SOCIAL_ENGINEERING',
+        'UNWANTED_SOFTWARE',
+        'POTENTIALLY_HARMFUL_APPLICATION',
+      ],
       platformTypes: ['ANY_PLATFORM'],
       threatEntryTypes: ['URL'],
       threatEntries: [{ url }],
@@ -50,37 +51,13 @@ async function validateUrlSafety(url) {
 
   const data = await response.json();
 
+  if (data.error) {
+    return { status: 'ERROR', message: 'Error validating URL', data };
+  }
+
   if (data.matches) {
-    return { status: 'Unsafe', message: 'Unsafe URL detected' };
+    return { status: 'UNSAFE', message: 'Unsafe URL detected', data };
   }
 
-  return { status: 'Safe', message: 'Safe URL' };
+  return { status: 'SAFE', message: 'Safe URL', data };
 }
-
-export default defineEventHandler(async (event) => {
-  const { req } = event.node;
-
-  // Apply rate limiting
-  const rateLimitResult = rateLimit(req);
-
-  if (rateLimitResult.status === 429) {
-    setResponseStatus(event, 429);
-    return { error: rateLimitResult.message };
-  }
-
-  try {
-    // Extract URL from request body
-    const body = await readBody(event);
-    const { url } = body;
-
-    if (!url) {
-      return { error: 'URL is required' };
-    }
-
-    // Validate the URL via Google Safe Browsing API
-    const validationResult = await validateUrlSafety(url);
-    return validationResult;
-  } catch (error) {
-    return { error: 'Error validating URL' };
-  }
-});
