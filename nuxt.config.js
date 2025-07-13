7aa2645110387811d0e224c0994a8f3f860aa8ac
Ismail9k <dev@ismail9k.com>
2025-07-13 19:05:57 +0300

    feat(nuxt): add Nitro configuration for Cloudflare Pages deployment, enabling enhanced compatibility and deployment options

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,14 @@ export default defineNuxtConfig({
     },
   },
 
+  nitro: {
+    preset: 'cloudflare_pages',
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+    },
+  },
+
   // pwa: {
   //   registerType: 'autoUpdate',
   //   manifest: {
